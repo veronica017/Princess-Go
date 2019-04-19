b70fddf5bb1927fe9a2dc8b1285a2cabc386ab66
veronica017 <u3532975@connect.hku.hk>
2019-04-20 03:19:43 +0800

    Finish Fungus communication between player and Wizard

---
+++ Assets/Scripts/TalkingWizard.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Fungus;
+
+
+public class TalkingWizard : MonoBehaviour
+{
+    
+    public string enterToDialogueBox;
+    public Flowchart talkChart;
+    public GameObject curtain;
+    public GameObject door;
+    public GameObject wizard;
+    private int talkingToWizard = 0;
+    //static int enterTimes=0;
+    private bool crystalBall = false;
+    void start(){
+        curtain.SetActive(true);
+        door.SetActive(false);
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (talkingToWizard==0)
+        {
+            Block target=talkChart.FindBlock(enterToDialogueBox);
+            talkChart.ExecuteBlock(target);
+            talkingToWizard++;
+        }
+        else if (talkingToWizard>=1 && talkingToWizard<=2 && !crystalBall)
+        {
+            Block target=talkChart.FindBlock("Busy");
+            talkChart.ExecuteBlock(target);
+            talkingToWizard++;
+        }
+        else{
+            Block target=talkChart.FindBlock("Finish");
+            talkChart.ExecuteBlock(target);
+            door.SetActive(true);
+           
+            //Destroy(curtain, 2);
+            //Destroy(gameObject, 2);
+
+        }
+    }
+
+}
