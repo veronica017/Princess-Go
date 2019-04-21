d38acb379dc367cb5104d0640df317fd7eb3a672
Yang Yafei <32016231+YangYafei1998@users.noreply.github.com>
2019-04-21 19:30:40 +0800

    Update DogController.cs

--- Assets/Scripts/DogController.cs
+++ Assets/Scripts/DogController.cs
@@ -1,34 +1,38 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Fungus;
 
 
-public class DogController: MonoBehaviour
+public class DogController : MonoBehaviour
 {
     public Flowchart talkChart;
-    private int x;
-   
-    void start(){
-        x=0;
+    //private int x;
+    static public bool hasBone;
+
+    void start()
+    {
+        //x = 0;
     }
     void OnTriggerEnter2D(Collider2D other)
     {
 
-        if (other.CompareTag("Player")){
-            if(x==0)
+        if (other.CompareTag("Player"))
+        {
+            if (!hasBone)
             {
-                Block target=talkChart.FindBlock("Dog");
+                Block target = talkChart.FindBlock("Dog");
                 talkChart.ExecuteBlock(target);
                 x++;
             }
-            else if(x==1){
-                Block target=talkChart.FindBlock("Disapear");
+            else if (hasBone)
+            {
+                Block target = talkChart.FindBlock("Disapear");
                 talkChart.ExecuteBlock(target);
                 x++;
             }
-            
+
         }
     }
 
