8ad3db33dabda2b5870f622f45f50d6f0bde767e
YangYafei1998 <faye.yangyafei@gmail.com>
2019-04-21 19:28:36 +0800

    no message

--- Assets/Scripts/DogController.cs
+++ Assets/Scripts/DogController.cs
@@ -5,34 +5,34 @@ using UnityEngine.SceneManagement;
 using Fungus;
 
 
-public class DogController: MonoBehaviour
+public class DogController : MonoBehaviour
 {
     public Flowchart talkChart;
-    //private int boneFound;
-    private Inventory inventory;
+    private int x;
     static public bool hasBone;
-   
-    void Start(){
-        //x=0;
-        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
-    }
 
+    void start()
+    {
+        x = 0;
+    }
     void OnTriggerEnter2D(Collider2D other)
     {
 
-        if (other.CompareTag("Player")){
-            if(!hasBone)
+        if (other.CompareTag("Player"))
+        {
+            if (!hasBone)
             {
-                Block target=talkChart.FindBlock("Dog");
+                Block target = talkChart.FindBlock("Dog");
                 talkChart.ExecuteBlock(target);
-                //x++;
+                x++;
             }
-            else{
-                Block target=talkChart.FindBlock("Disapear");
+            else if (hasBone)
+            {
+                Block target = talkChart.FindBlock("Disapear");
                 talkChart.ExecuteBlock(target);
-                //x++;
+                x++;
             }
-            
+
         }
     }
 
