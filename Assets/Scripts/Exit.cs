ef308b0d69f6400fc4b6e61db922be7744f95148
veronica017 <u3532975@connect.hku.hk>
2019-04-21 11:14:00 +0800

    the conversation with sodier in the hall

---
+++ Assets/Scripts/Exit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Fungus;
+
+
+public class Exit : MonoBehaviour
+{
+    public Flowchart talkChart;
+   
+    void start(){
+        
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.CompareTag("Player")){
+            Block target=talkChart.FindBlock("Soldier");
+            talkChart.ExecuteBlock(target);
+        }
+    }
+
+}
